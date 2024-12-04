1cb02efe0e17e4481861886914c683f853d7f167
izzakhus <izzakhus@gmail.com>
2024-12-04 15:16:10 +0600

    lesson-16

--- testapp/models.py
+++ testapp/models.py
@@ -10,7 +10,6 @@ class AdvUser(models.Model):
 class Spare(models.Model):
     name = models.CharField(max_length=30)
 
-
 class Machine(models.Model):
     name = models.CharField(max_length=30)
     spares = models.ManyToManyField(Spare)
