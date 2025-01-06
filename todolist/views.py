8cac6e0d1b585eee876f3e0a1b1490e0743d7e06
izzakhus <izzakhus@gmail.com>
2025-01-07 01:42:06 +0600

    hw-16

--- todolist/views.py
+++ todolist/views.py
@@ -4,26 +4,6 @@ from django.urls import reverse_lazy
 from .models import Task
 
 
-def todo_list(request):
-    pass
-
-
-def todo_detail(request, todo_id):
-    pass
-
-
-def todo_create(request):
-    pass
-
-
-def todo_update(request, todo_id):
-    pass
-
-
-def todo_delete(request, todo_id):
-    pass
-
-
 class TaskListView(ListView):
     model = Task
     template_name = 'todolist/task_list.html'
